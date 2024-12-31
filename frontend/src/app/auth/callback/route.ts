68b01b7448a4a84efb52d1679d53decd3f8be062
Aman Kumar Bairagi <amanbairagi1089@gmail.com>
2025-01-01 01:28:04 +0530

    [FIX] : small change 5

--- frontend/src/app/auth/callback/route.ts
+++ frontend/src/app/auth/callback/route.ts
@@ -10,15 +10,7 @@ export async function GET(request: Request) {
     const supabase = await createClient();
     const { error } = await supabase.auth.exchangeCodeForSession(code);
     if (!error) {
-      const forwardedHost = process.env.NEXT_PUBLIC_URL;
-      const isLocalEnv = process.env.NODE_ENV === "development";
-      if (isLocalEnv) {
-        return NextResponse.redirect(`${origin}${next}`);
-      } else if (forwardedHost) {
-        return NextResponse.redirect(`${forwardedHost}${next}`);
-      } else {
-        return NextResponse.redirect(`${origin}${next}`);
-      }
+      return NextResponse.redirect(`${origin}${next}`);
     }
   }
 
