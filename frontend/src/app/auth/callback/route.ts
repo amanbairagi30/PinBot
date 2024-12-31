d8dd9489f3b9d3b37f27d5a35bb0f4009fa7870e
Aman Kumar Bairagi <amanbairagi1089@gmail.com>
2025-01-01 01:24:02 +0530

    [FIX] : small change 4

--- frontend/src/app/auth/callback/route.ts
+++ frontend/src/app/auth/callback/route.ts
@@ -15,7 +15,7 @@ export async function GET(request: Request) {
       if (isLocalEnv) {
         return NextResponse.redirect(`${origin}${next}`);
       } else if (forwardedHost) {
-        return NextResponse.redirect(`https://${forwardedHost}${next}`);
+        return NextResponse.redirect(`${forwardedHost}${next}`);
       } else {
         return NextResponse.redirect(`${origin}${next}`);
       }
