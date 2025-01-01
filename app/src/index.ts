31ea27e7fc5e651ce48d2b970aee83f2e37c0ac0
Aman Kumar Bairagi <amanbairagi1089@gmail.com>
2025-01-01 17:44:46 +0530

    fix: small fix

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,9 +1,28 @@
+import express, { json } from "express";
 import { Events } from "discord.js";
 import "dotenv/config.js";
 import { onReady } from "./events/onReady";
 import { onInteraction } from "./events/onInteraction";
 import { Bot } from "./config/initClient";
+import cors from "cors";
 
+const app = express();
+app.use(json());
+app.use(cors());
+const port = process.env.PORT || 5000;
+
+// Set up the Express server
+app.get("/", (req, res) => {
+  res.send("SERVER is UP!");
+  console.log("SERVER is UP!");
+});
+
+// Start the Express server
+app.listen(port, () => {
+  console.log("Listening at port:", port);
+});
+
+// Set up the Discord bot
 (async () => {
   Bot.on("ready", (client) => onReady(client));
   Bot.on(
